24643ce32be4b654630cdc2a8a41a85f302110c7
Chris Featherstone <featherstone.chris@gmail.com>
2021-12-27 12:53:12 -0700

    add pane for traefik

--- tools/startup.sh
+++ tools/startup.sh
@@ -11,10 +11,11 @@ tmux new-session -d -s docker-compose
 tmux set-option -t docker-compose set-remain-on-exit on
 
 # Running the script in a new window starts process in tmux
-tmux new-window -d -n 'media' -t docker-compose:1 'docker-compose -f /home/media/nzb.compose up'
-tmux new-window -d -n 'monitoring' -t docker-compose:2 'docker-compose -f /home/media/monitoring.compose up'
-tmux new-window -d -n 'unifi' -t docker-compose:3 'docker-compose -f /home/media/unifi.compose up'
-tmux new-window -d -n 'urbackup' -t docker-compose:4 'docker-compose -f /home/media/urbackup.compose up'
-tmux new-window -d -n 'timemachine' -t docker-compose:5 'docker-compose -f /home/media/timemachine.compose up'
+tmux new-window -d -n 'traefik' -t docker-compose:1 '/usr/local/bin/docker-compose -f /home/media/traefik up'
+tmux new-window -d -n 'media' -t docker-compose:2 '/usr/local/bin/docker-compose -f /home/media/nzb.compose up'
+tmux new-window -d -n 'monitoring' -t docker-compose:3 '/usr/local/bin/docker-compose -f /home/media/monitoring.compose up'
+tmux new-window -d -n 'unifi' -t docker-compose:4 '/usr/local/bin/docker-compose -f /home/media/unifi.compose up'
+tmux new-window -d -n 'urbackup' -t docker-compose:5 '/usr/local/bin/docker-compose -f /home/media/urbackup.compose up'
+tmux new-window -d -n 'timemachine' -t docker-compose:6 '/usr/local/bin/docker-compose -f /home/media/timemachine.compose up'
 
 exit 0
